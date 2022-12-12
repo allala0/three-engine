024001e3436949d56cd1fd22fd88acff768ce42a
Artur Brytkowski <96352398+allala0@users.noreply.github.com>
2022-12-12 18:19:02 +0100

    fixed clone in AnimatedMaterial

--- src/tools/objects/materials/AnimatedMaterial.js
+++ src/tools/objects/materials/AnimatedMaterial.js
@@ -199,7 +199,7 @@ class AnimatedMaterial extends ShaderMaterial{
     }
 
     clone(){
-        return new AnimatedMaterial(this.spriteSheet, this.spriteSheet.tileCount.clone(), {...this.parameters});
+        return new AnimatedMaterial(this.spriteSheet, {...this.parameters});
     }
 }
 
